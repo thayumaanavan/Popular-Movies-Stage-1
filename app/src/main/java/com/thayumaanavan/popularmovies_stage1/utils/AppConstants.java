b5a881a07457dfbb18e548d46a7d1d99c2a300b4
thayumaanavan <thayumaanavan@hotmail.com>
2018-06-18 20:40:19 +0530

    secured API key via gradle properties

--- app/src/main/java/com/thayumaanavan/popularmovies_stage1/utils/AppConstants.java
+++ app/src/main/java/com/thayumaanavan/popularmovies_stage1/utils/AppConstants.java
@@ -1,9 +1,11 @@
 package com.thayumaanavan.popularmovies_stage1.utils;
 
+import com.thayumaanavan.popularmovies_stage1.BuildConfig;
+
 public class AppConstants {
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String API_KEY = "api_key";
-    public static final String API_KEY_VALUE = "YOUR_API_KEY";
+    public static final String API_KEY_VALUE = BuildConfig.API_KEY;
 
     public static final String POSTER_BASE_URL = "http://image.tmdb.org/t/p/w185";
     public static final String BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w342";
